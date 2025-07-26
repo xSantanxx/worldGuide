ca034e177b3b20ec99eb4a431bbaaea5fd96717d
xSantanxx <dmitripower@yahoo.com>
2025-07-26 02:39:42 -0400

    customized title

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello World!!!</h1>
+      <body className='bg-indigo-500'>
+        <div className='h-20 w-100 top-25 left-80 absolute rounded-[10vw] shadow-lg shadow-sky-500/50 bg-sky-500 border-2 border-solid border-sky-500 hover:bg-sky-700 cursor-default text-center p-2 shadow-xl wrap-break-word'>
+          <h1 className='text-shadow-lg/30 text-white-600'>Travel Guide</h1>
+        </div>
+      </body>
     </>
   )
 }
